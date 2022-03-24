463219908491a359deaf6669cf20996ca591fcf6
Marcus <marcanthonyzpadilla@gmail.com>
2022-03-24 11:17:09 -0700

    add if/elseif/else function
    Co-authored-by: Mark McConnell <amark.mcconnell@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,24 @@
+
 $(document).ready(function() {
   const age = parseInt(prompt("How old are you?"));
+  const pres = prompt("Pick One President: Biden, Trump, or Obama");
+
+  const showPres = function () {
+    if (pres === "Biden" || pres === "biden") {
+      $("#biden").show();
+      } else if (pres === "Trump" || pres === "trump") {
+        $("#trump").show();
+      } else {
+        $("#obama").show();
+    };
+  }
 
   if (age >= 18) {
     $('#vote').show();
+    showPres();
   } else {
     $('#under-18').show();
+    showPres();
   }
+  
 });
